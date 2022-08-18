9516d844af966ad6426eb5cba8f6c2039d95e6ee
Chris Drackett <chris@drackett.com>
2022-08-18 12:53:22 -0700

    get storybook running on its own

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -12,6 +12,7 @@ const config: StorybookConfig = {
   },
   typescript: {
     check: false,
+    skipBabel: false
   },
 }
 
--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,6 +1,5 @@
 import React from 'react'
-import { Button as NativeButton } from 'react-native'
 
 export const Button = () => {
-  return <NativeButton title="button" onPress={() => {}} />
+  return <div>test</div>
 }
