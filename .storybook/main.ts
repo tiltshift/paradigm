3d5130db2e7a530ca1b6827ac2b2140636d80d04
Chris Drackett <chris@drackett.com>
2023-05-10 13:10:20 -0700

    upgrade storybook to 7

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,24 +1,23 @@
-import type { StorybookConfig } from '@storybook/core-common'
-
+import type { StorybookConfig } from '@storybook/core-common';
 const config: StorybookConfig = {
   stories: ['../src/**/*.stories.tsx'],
-  addons: [
-    '@storybook/addon-links',
-    '@storybook/addon-essentials',
-    '@storybook/addon-interactions',
-    '@storybook/addon-react-native-web',
-  ],
-  framework: '@storybook/react',
-  core: {
-    builder: '@storybook/builder-webpack5',
+  addons: ['@storybook/addon-links', '@storybook/addon-essentials', '@storybook/addon-interactions', '@storybook/addon-react-native-web'],
+  framework: {
+    name: '@storybook/react-webpack5',
+    options: {
+      fastRefresh: false
+    }
   },
+  core: {},
   typescript: {
-    check: false,
+    check: false
   },
   // @ts-ignore this isn't correct in the types
   reactOptions: {
-    fastRefresh: false,
+    fastRefresh: false
   },
-}
-
-module.exports = config
+  docs: {
+    autodocs: true
+  }
+};
+module.exports = config;
