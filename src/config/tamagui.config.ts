1fdcc97c47ebcce7a6481239a6404905479c3a55
Chris Drackett <chris@drackett.com>
2026-02-11 14:22:11 -0800

    update to tamagui 2.0

--- src/config/tamagui.config.ts
+++ src/config/tamagui.config.ts
@@ -1,4 +1,4 @@
-import { defaultConfig as defaultTamaguiConfig } from "@tamagui/config/v4"
+import { defaultConfig as defaultTamaguiConfig } from "@tamagui/config/v5"
 import { px } from "@tamagui/core"
 import { type CreateTamaguiProps, createTamagui, createTokens } from "tamagui"
 
